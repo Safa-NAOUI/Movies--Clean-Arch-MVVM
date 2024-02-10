6edcef7444b19145006e3d0e72d7aee985322f14
Safa <safa.naoui25@gmail.com>
2024-02-10 22:05:35 +0100

    test

--- domain/src/main/java/com/example/domain/repo/EpisodeRepository.kt
+++ domain/src/main/java/com/example/domain/repo/EpisodeRepository.kt
@@ -5,6 +5,7 @@ import com.example.domain.model.EpisodesModel
 /**
  * Created by Safa NAOUI on 10,February,2024
  */
+
 interface EpisodeRepository {
      suspend fun getEpisodes(page: Int): List<EpisodesModel>
 }
